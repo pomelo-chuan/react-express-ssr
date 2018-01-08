46ba6fbc3d1de3d02ecc38f70695a05d810a17d0
chenchuan <chenchuan@wosai-inc.com>
2018-01-08 20:08:59 +0800

    dd

---
+++ ssr_src/app/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Comment {
+    render () {
+        return (
+            <div>
+                <h1>Hello World!</h1>
+            </div>
+        );
+    }
+}
